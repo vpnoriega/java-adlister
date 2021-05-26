1d7e4c6fb2397682617e5ab9043904dfe1c2d60b
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-05-26 11:44:49 -0500

    Complete: Servelet Ex and Bonus

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,26 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.getWriter().println("<h1>Hello, World!</h1>");
+
+// BONUS: Make the page say "Hello, <name>!" if name is passed as part of the query string (e.g. /hello?name=codeup), otherwise default to "Hello, World!".:
+
+        String name = request.getParameter("name");
+
+        if(name == null){
+            name = "Hello world!";
+        }
+
+        response.getWriter().println("<h1>Hello " + name + "!" + "</h1>");
+
+
 
     }
 }
