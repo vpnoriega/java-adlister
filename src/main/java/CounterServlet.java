793e887406e499fee1adb318fce427baf1ce5d4a
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-05-26 12:38:24 -0500

    Completed: Bonus CounterServlet

--- src/main/java/CounterServlet.java
+++ src/main/java/CounterServlet.java
@@ -7,14 +7,24 @@ import java.io.IOException;
 @WebServlet(name = "CounterServlet", urlPatterns = "/count")
 public class CounterServlet extends HttpServlet {
 
-    private int hitCount = 1;
+    private int hitCount = 0;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-        response.setContentType("text/html"); //the browser needs to know what to run
+        hitCount++;
 
+        String reset = request.getParameter("reset");
+
+        if(reset != null){
+            hitCount = 1;
+
+            response.setContentType("text/html"); //the browser needs to know what to run
+            response.getWriter().println("<h2> This page count has been reset to: " + hitCount + "</h2>");
+        }
+
+        response.setContentType("text/html"); //the browser needs to know what to run
         response.getWriter().println("<h2> This page has been visited: " + hitCount + " times! </h2>");
 
-        hitCount++;
+
     }
 }
