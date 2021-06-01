19bcee47353e5feef27ea8be35863ad7eac1d601
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-06-01 14:07:05 -0500

    In progress: Javabeans exercise

---
+++ src/main/java/Album.java
@@ -0,0 +1,11 @@
+import java.io.Serializable;
+
+public class Album implements Serializable {
+    private int id;
+    private String artist;
+    private String name;
+    private int release_date;
+    private float sales;
+    private String genre;
+
+}
